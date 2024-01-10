abf6b02a2d5ddfaa59e76dc13ba755b26fdbe20a
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-01-10 22:14:43 +0530

    added user register controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,30 +1,72 @@
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse";
-import {User} from "../modles/user.model.js"
-import {uploadOnCloudnary , deleteFromCloudinary} from "../utils/cloudinary.js"
-import jwt from "jsonwebtoken"
+import { User } from "../modles/user.model.js";
+import {
+  uploadOnCloudnary,
+  deleteFromCloudinary,
+} from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import fs from "fs";
 
-const registerUser  = asyncHandler(async(req , res) => {
-    const {username , email , fullName , password} = req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, fullName, password } = req.body;
 
-    if(
-        [username , email , fullName , password].some((entries) => entries.length === 0 ? true : false)
-    ){
-        return new ApiError(400 , "All fields are required!")
-    }
+  if (
+    [username, email, fullName, password].some((entries) =>
+      entries.length === 0 ? true : false
+    )
+  ) {
+    return new ApiError(400, "All fields are required!");
+  }
 
-   const userExists = await User.exists({username , email})
+  const ifUserExists = await User.exists({ username, email });
 
-   if(userExists){
-    return new ApiError(400 , 'Email or username already exists')
-   }
+  if (ifUserExists) {
+    return new ApiError(400, "Email or username already exists");
+  }
 
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+  if (!(avatarLocalPath && coverImageLocalPath)) {
+    throw new ApiError(400, "Avatar and cover image are required fields");
+  }
 
-})
+  const avatar = await uploadOnCloudnary(avatarLocalPath);
+  const coverImage = await uploadOnCloudnary(coverImageLocalPath);
 
-export {
-    registerUser
-}
+  if (!(avatar && coverImage)) {
+    //delete local file after cloudinary error
+    fs.unlinkSync(avatarLocalPath);
+    fs.unlinkSync(coverImageLocalPath);
+    throw new ApiError(
+      500,
+      "Something went wrong while uploading the files to cloudinary"
+    );
+  }
+
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage.url,
+    username: username.toLowerCase(),
+  });
+
+  const newUser = await User.findById(user._id).select(
+    "-password  -refreshToken"
+  );
+
+  if (!newUser) {
+    throw new ApiError(500, "Something went wrong while regestring the user");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, newUser, "User registered successfully"));
+});
+
+export { registerUser };

8af6bd13bfbce9bb879e396308429637cfbd8fbe
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-01-19 22:47:01 +0530

    added the user authentication middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,8 +1,29 @@
+import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../modles/user.model.js";
+import jwt from "jsonwebtoken";
 
-const authenticateUser = asyncHandler(async(req , res) => {
+const authenticateUser = asyncHandler(async (req, _ , next) => {
+  try {
+    const accessToken = req.cookies?.accessToken;
+    if (!accessToken) {
+      throw new ApiError(401, "No access token exists");
+    }
+    const decodeToken = jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const userId = decodeToken._id;
+    const user = User.findById(userId).select("-password  -refreshToken");
 
-  
-})
+    if (!user) {
+      throw new ApiError(401, "user does not exist");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
 
-export default authenticateUser
+export default authenticateUser;
